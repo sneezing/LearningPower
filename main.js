091bfcb814b59db4651457d7830a3f2c7a5b6e81
miugod <2513043962@qq.com>
2019-06-04 17:40:47 +0800

    修复看视频

--- main.js
+++ main.js
@@ -5,16 +5,25 @@
  * @LastEditors: Veagau
  * @Date: 2019-03-27 15:49:14
  * @LastEditTime: 2019-03-31 17:45:22
+ * @Modify: MiuGod
+ * @DateOfModify: 2019-06-02 17:21:30
  */
 //console.show();//调试窗口
 
 auto.waitFor();//辅助权限授予
 auto.setMode("normal");
 //全局变量定义
-var vTimeTotal = 20;//视频学习目标时间（秒），默认视频学习时长25分钟（25*60）
+const DEBUG = true;
+const WIDTH = Math.min(device.width, device.height);
+const HEIGHT = Math.max(device.width, device.height);
+var vTimeTotal = DEBUG ? 1 : 180;//视频学习目标时间（秒），默认视频学习时长6分钟（18*60）
 var sTimeTotal = 10;//视频分享目标时间（秒），默认视频分享时长10秒
-var loops = 3;//视频分享次数，默认分享6次
-var rTimeTotal = 10;//文章阅读目标时间（秒），默认文章阅读时长16分钟（16*60）
+var loops = 1;//视频分享次数，默认分享2次
+var rTimeTotal = DEBUG ? 3 : 120;//文章阅读目标时间（秒），默认文章阅读时长12分钟（12*60）
+
+if (DEBUG) {
+    console.show();
+}
 
 /**
  * @name:延迟函数ms→s 
@@ -57,6 +66,9 @@ function popupDeal(params) {
     while (text("重新播放").exists()) {
         text("重新播放").click();
     }
+    while (text("取消分享").exists()) {
+        text("取消分享").click();
+    }
     return true;
 }
 
@@ -67,7 +79,7 @@ function popupDeal(params) {
  */
 function watchTimer(time) {
     for (var timer = 0; timer < time;) {
-        toSDelay(5);
+        toSDelay(DEBUG ? 1 : 5);
         timer += 5
         if (timer <= 60) {
             toast("已学习" + timer + "秒");
@@ -90,11 +102,11 @@ function wechatShare(loop) {
     for (var i = 1; i <= loop; i++) {
         while (!text("观点").exists());
         toast("开始分享第" + i + "/" + loop + "次");
-        var shareIcon = classNameContains("ImageView").depth(2).findOnce(2);
+        var shareIcon = classNameContains("ImageView").depth(10).drawingOrder(4);
         if(shareIcon.click()==true){
             print("点击分享按钮");
         }
-        toSDelay(5);
+        toSDelay(2);
         while (!textContains("分享给微信").exists());
         if(click("分享给微信\n好友")==true){
             toast("跳转微信中……");
@@ -102,6 +114,7 @@ function wechatShare(loop) {
         while (!text("多选").exists());//等待微信界面载入
         toSDelay(2);
         back();
+        popupDeal();
         toSDelay(3);
     }
     toast("视频分享任务完成");
@@ -114,11 +127,11 @@ function wechatShare(loop) {
  * @return: 
  */
 function videoLike() {
-    var starIcon = classNameContains("ImageView").depth(2).findOnce(1);
-    if (starIcon.click() == true) {
-        popupDeal();//处理首次收藏提示弹窗
-        toast("收藏成功");
-        toSDelay(5);
+    var starIcon = classNameContains("ImageView").depth(10).drawingOrder(3);
+    for (var i=1;i<5;i++) {
+        toSDelay(2);
+        starIcon.click();
+        popupDeal();
     }
     return true;
 }
@@ -129,17 +142,69 @@ function videoLike() {
  * @return: 
  */
 function videoWatch() {
-    if (click("央视网", 0) == true) {
-        toast("进入新闻联播");
-        toSDelay(5);
+    console.log("开始视听学习");
+
+    // 搜索联播频道框
+    function getYsws(){
+        var layer = text("联播频道").findOnce();
+        function dfs(uiObj) {
+            if (uiObj.text() == "央视网") {
+                return true;
+            }
+            var ok = false;
+            uiObj.children().every(element => {
+                ok = ok || dfs(element);
+                return !ok;
+            });
+            return ok;
+        }
+        while (true) {
+            layer = layer.parent();
+            if (dfs(layer)) {
+                break;  // 如果dfs找到央视网则跳出
+            }
+        }
+        return layer.children().find(text("央视网"));
     }
-    popupDeal();
-    if(watchTimer(vTimeTotal)==true){
-        toSDelay(2);
-        toast("视频观看完成");
-    } 
-    toSDelay(5);
-    back();
+    
+
+    // 开始看视频
+    var count = 0;  // 央视网s的计数器
+    var ysws = getYsws();
+    for(var i = 0; i < 6; i++){
+        console.log("watching videos");
+        // console.verbose(ysws.get(i));
+        var b = ysws.get(count).bounds();
+        // console.verbose(b);
+        if (click(b.centerX(), b.centerY()) == true) {
+            toastLog("进入第"+(i+1)+"个视频");
+            toSDelay(1);
+            count++;
+        }
+        popupDeal();
+        if(watchTimer(vTimeTotal)==true){
+            toSDelay(1);
+            toastLog((i+1)+"条视频观看完成");
+        }     
+        if (i < 5){
+            back();
+        } else {    // 最后一次分享收藏
+            wechatShare();
+            toSDelay(2);
+            videoLike();
+            back();
+            break; 
+        }       
+        if (count == ysws.size()-1){
+            toSDelay(3);
+            console.log("xiahua");
+            swipe(WIDTH/2, HEIGHT/5*4, WIDTH/2, HEIGHT/5, 1000);
+            ysws = getYsws();
+            count = 1;
+        } 
+              
+        toSDelay(3);
+    }
     return true;
 }
 
@@ -148,35 +213,25 @@ function videoWatch() {
  * @param {none} 
  * @return: none
  */
-function videoShare() {
-    if (click("央视网", 1) == true) {
-        toast("进入第二条视频新闻");
-        toSDelay(5);
-    }
-    popupDeal();
-    if(watchTimer(sTimeTotal)==true){
-        toSDelay(2);
-        toast("第二条视频观看完成");
-    } 
-    toSDelay(5);
-    back();
-    toSDelay(3);
-    if (click("央视网", 2) == true) {
-        toast("进入第三条视频新闻");
-        toSDelay(5);
-    }
-    popupDeal();
-    if(watchTimer(sTimeTotal)==true){
-        toSDelay(2);
-        toast("第三条视频观看完成");
-    } 
-    toSDelay(5);
-    wechatShare(loops);
-    toSDelay(2);
-    videoLike();
-    back();
-    return true;
-}
+// function videoShare() {
+//     console.log("videowatch: start.");
+//     if (click("央视网", 0) == true) {
+//         toastLog("进入第1条视频新闻");
+//         toSDelay(5);
+//     }
+//     popupDeal();
+//     if(watchTimer(vTimeTotal)==true){
+//         toSDelay(2);
+//         toastLog("第1条视频观看完成");
+//     } 
+//     console.log("VideoShare: start.");
+//     wechatShare(2);
+//     toSDelay(2);
+//     videoLike();  
+//     back();
+//     toSDelay(5);
+//     return true;
+// }
 
 /**
  * @name: 视频学习
@@ -185,7 +240,7 @@ function videoShare() {
  */
 function videoStudy() {
     while (!desc("学习").exists());
-    if (click("视频学习") == true) {
+    if (click("视听学习") == true) {
         toast("开始视频学习");
     }
     toSDelay(2);
@@ -193,9 +248,9 @@ function videoStudy() {
         toast("进入联播频道");
     }
     toSDelay(5);
+    // videoShare();
     videoWatch();
     toSDelay(5);
-    videoShare();
     return true;
 }
 
@@ -214,18 +269,21 @@ function newsStudy() {
         toast("进入要闻模块");
         toSDelay(3);
     }
-    var i = 0;
-    if (click("“学习强国”学习平台", i) == true) {
-        var count = i + 1;
-        toast("开始阅读第" + count + "篇要闻……");
-        toSDelay(3);
-    }
-    if(watchTimer(rTimeTotal)==true){
-        toSDelay(2);
-        toast("文章阅读完成");
-    }
-    toSDelay(5);
-    back();
+    for(var i=0;i<6;i++){
+        console.log("Begin reading.");
+        if (click("“学习强国”学习平台", i) == true) {
+            toast("开始阅读第" + (i+1) + "篇要闻……");
+            toSDelay(3);
+        }
+        popupDeal();
+        if(watchTimer(rTimeTotal)==true){
+            toSDelay(2);
+            toast((i+1)+"篇文章阅读完成");
+        }
+        
+        back();
+        toSDelay(5);
+    }    
 }
 
 initScript();
